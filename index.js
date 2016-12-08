499832f4d6be29e77f239e16a99d13f0a6815742
Henrik Ruscon <henrik.dahlheim@gmail.com>
2016-12-08 13:14:28 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,180 @@
+// Require
+const { shell } = require('electron');
+const { exec } = require('child_process');
+
+// Config
+exports.decorateConfig = config => {
+    return Object.assign({}, config, {
+        css: `
+            ${config.css || ''}
+            .terms_terms {
+                margin-bottom: 30px;
+            }
+            .footer_footer {
+                display: flex;
+                justify-content: space-between;
+                position: fixed;
+                bottom: 0;
+                left: 0;
+                right: 0;
+                z-index: 100;
+                font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
+                font-size: 12px;
+                height: 30px;
+                padding: 0 14px 1px;
+                opacity: 0.45;
+                cursor: default;
+                -webkit-user-select: none;
+                transition: opacity 250ms ease;
+            }
+            .footer_footer:hover {
+                opacity: 1;
+            }
+            .footer_footer::before {
+                content: '';
+                position: absolute;
+                top: 0;
+                left: 0;
+                z-index: -1;
+                width: 100%;
+                height: 100%;
+                border-bottom-left-radius: 4px;
+                border-bottom-right-radius: 4px;
+                background-color: ${config.foregroundColor || 'transparent'};
+                opacity: 0.07;
+            }
+            .item_item {
+                position: relative;
+                display: flex;
+                align-items: center;
+                color: ${config.foregroundColor || 'white'};
+                background-repeat: no-repeat;
+                background-position: left center;
+                white-space: nowrap;
+                overflow: hidden;
+                text-overflow: ellipsis;
+                opacity: 0;
+                pointer-events: none;
+            }
+            .item_active {
+                opacity: 0.7;
+                pointer-events: auto;
+            }
+            .item_active::before {
+                content: '';
+                position: absolute;
+                left: 0;
+                width: 14px;
+                height: 100%;
+                -webkit-mask-repeat: no-repeat;
+                -webkit-mask-position: left center;
+                background-color: ${config.foregroundColor || 'white'};
+            }
+            .item_folder {
+                padding-left: 21px;
+                cursor: pointer;
+            }
+            .item_folder:hover {
+                text-decoration: underline;
+            }
+            .item_folder::before {
+                -webkit-mask-image: url('${__dirname}/icons/folder.svg');
+                -webkit-mask-size: 14px 12px;
+            }
+            .item_branch {
+                padding-left: 16px;
+            }
+            .item_branch::before {
+                -webkit-mask-image: url('${__dirname}/icons/branch.svg');
+                -webkit-mask-size: 9px 12px;
+            }
+        `
+    });
+};
+
+let curPid;
+let curCwd;
+let curBranch;
+let uids = {};
+
+// Current shell cwd
+const setCwd = (pid) =>
+    exec(`lsof -p ${pid} | grep cwd | tr -s ' ' | cut -d ' ' -f9-`, (err, cwd) => {
+        cwd = cwd.trim();
+        curCwd = cwd;
+
+        store.dispatch({
+            type: 'SESSION_SET_CWD',
+            cwd
+        });
+});
+
+// Current git branch
+const setBranch = (actionCwd) => {
+    exec(`git rev-parse --abbrev-ref HEAD`, { cwd: actionCwd }, (err, branch) => {
+        curBranch = branch;
+    })
+};
+
+// Status line
+exports.decorateHyper = (Hyper, { React }) => {
+    return class extends React.Component {
+        constructor(props) {
+            super(props);
+            this.state = {
+                folder: curCwd,
+                branch: curBranch
+            };
+            this.handleClick = this.handleClick.bind(this);
+        };
+        handleClick() {
+            shell.openExternal('file://'+this.state.folder);
+        };
+        render() {
+            const hasBranch = this.state.branch !== '' ? 'item_active' : '';
+
+            return (
+                React.createElement(Hyper, Object.assign({}, this.props, {
+                    customChildren: React.createElement('footer', { className: 'footer_footer' },
+                        React.createElement('div', { className: 'item_item item_folder item_active', onClick: this.handleClick }, this.state.folder),
+                        React.createElement('div', { className: `item_item item_branch ${hasBranch}` },  this.state.branch)
+                    )
+                }))
+            );
+        };
+        componentDidMount() {
+            setInterval(() => this.setState({
+                folder: curCwd,
+                branch: curBranch
+            }), 100);
+        };
+    };
+};
+
+// Sessions
+exports.middleware = (store) => (next) => (action) => {
+    switch (action.type) {
+        case 'SESSION_PTY_DATA':
+            if (curPid && uids[action.uid] === curPid) setCwd(curPid);
+            break;
+        case 'SESSION_ADD':
+            uids[action.uid] = action.pid;
+            curPid = action.pid;
+            setCwd(curPid);
+            break;
+        case 'SESSION_SET_CWD':
+            setBranch(curCwd);
+            break;
+        case 'SESSION_SET_ACTIVE':
+            curPid = uids[action.uid];
+            setCwd(curPid);
+            break;
+        case 'SESSION_PTY_EXIT':
+            delete uids[action.uid];
+            break;
+        case 'SESSION_USER_EXIT':
+            delete uids[action.uid];
+            break;
+    }
+    next(action);
+};
---
+++ readme.md
@@ -0,0 +1,22 @@
+# hyper-statusline [![npm](https://img.shields.io/npm/v/hyper-statusline.svg?maxAge=86400?style=flat-square)](https://www.npmjs.com/package/hyper-statusline) [![npm](https://img.shields.io/npm/dt/hyper-statusline.svg?maxAge=86400?style=flat-square)](https://www.npmjs.com/package/hyper-statusline)
+
+> Status Line Plugin for [Hyper](https://hyper.is)
+
+![](screen.png)
+
+![](screen_status.png)
+
+
+## Install
+
+Add `hyper-statusline` after your theme to the plugins array in your `~/.hyper.js` config file and full reload.
+
+
+## Theme
+
+* [hyperterm-chesterish](https://github.com/henrikdahl/hyperterm-chesterish)
+
+
+## License
+
+MIT © Henrik Dahlheim
