d638d512e7e8bf1f4235d5302e47e99fce5fbd87
Henrik Ruscon <henrik.dahlheim@gmail.com>
2017-07-17 11:33:23 +0200

    define default colors

--- index.js
+++ index.js
@@ -5,10 +5,29 @@ const tildify = require('tildify');
 
 // Config
 exports.decorateConfig = (config) => {
+    const configColors = Object.assign({
+        black: '#000000',
+        red: '#ff0000',
+        green: '#33ff00',
+        yellow: '#ffff00',
+        blue: '#0066ff',
+        magenta: '#cc00ff',
+        cyan: '#00ffff',
+        white: '#d0d0d0',
+        lightBlack: '#808080',
+        lightRed: '#ff0000',
+        lightGreen: '#33ff00',
+        lightYellow: '#ffff00',
+        lightBlue: '#0066ff',
+        lightMagenta: '#cc00ff',
+        lightCyan: '#00ffff',
+        lightWhite: '#ffffff',
+    }, config.colors);
+
     const hyperStatusLine = Object.assign({
         footerTransparent: true,
-        dirtyColor: config.colors.lightYellow,
-        arrowsColor: config.colors.blue,
+        dirtyColor: configColors.lightYellow,
+        arrowsColor: configColors.blue,
         fontSize: 12,
     }, config.hyperStatusLine);
 
